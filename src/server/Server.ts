b17597525efd6c05ddaf4cbf431fbfe936412711
Yuri Santana <yuri.luana2016@gmail.com>
2024-10-16 18:01:26 -0300

    add cors

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import "dotenv/config";
+import cors from "cors";
 
 import "../shared/services/TraducoesYup";
 import { router } from "./routes";
 
 const server = express();
+
+server.use(cors())
 
 server.use(express.json());
 
